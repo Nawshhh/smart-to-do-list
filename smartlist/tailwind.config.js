f1f9ad5ece6d49ad1b13d878080ec7a745715306
Nawshhh <wilbertshawnvillagarcia@gmail.com>
2025-08-12 18:59:42 +0800

    feat: implemented input for user

---
+++ smartlist/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { 
+    extend: {
+      fontFamily: {
+        helvetica: ['"Helvetica Neue"', "Helvetica", "Arial", "system-ui", "sans-serif"],
+      }
+    } 
+  },
+  plugins: [],
+}
