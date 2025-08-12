80d3f15695b4ddac15579848c6b2fe2a078ac619
Nawshhh <wilbertshawnvillagarcia@gmail.com>
2025-08-12 23:07:46 +0800

    fix: fixed navigation bar

--- smartlist/src/components/Body.jsx
+++ smartlist/src/components/Body.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Body(props) {
   return (
-    <div className='min-w-[1608px] min-h-[768px] bg-slate-500' >
+    <div className='min-w-[1608px] min-h-[768px]' >
         {props.children}
     </div>
   )
