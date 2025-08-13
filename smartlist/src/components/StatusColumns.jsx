a2f76b4c0466e68a60a9401942ee4a08f4d6b73d
Nawshhh <wilbertshawnvillagarcia@gmail.com>
2025-08-14 02:27:22 +0800

    fix: added task UI

--- smartlist/src/components/StatusColumns.jsx
+++ smartlist/src/components/StatusColumns.jsx
@@ -4,8 +4,8 @@ function StatusColumns(props) {
   return (
     <div className='h-[768px] w-[500px]'>
         <p className='font-helvetica font-bold text-[30px] text-white mb-[10px]'>{props.status}</p>
-        <div className='w-[500px] h-[720px] bg-[#545454] rounded-[20px]'>
-
+        <div className='w-[500px] h-[720px] bg-[#545454] rounded-[20px] flex flex-col items-center py-[18px] gap-y-[20px] overflow-auto scrollbar-none'>
+            {props.children}
         </div>
     </div>
   )
--- smartlist/src/components/Task.jsx
+++ smartlist/src/components/Task.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 
 function Task() {
-  return (
-    <div>Task</div>
-  )
+    const tasks = [
+        {
+            task_name: "Test",
+            task_priority: 1,
+            task_completion_date: "January 24, 2024"
+        }
+    ]
+
+    const priorityMap = { 1: "High", 2: "Medium", 3: "Low" };
+    const priority = priorityMap[tasks[0].task_priority] ?? "";
+
+    const bgMap = { 1: "bg-[#DE3D3D]", 2: "bg-[#3DDE3D]", 3: "bg-[#DEDE3D]" };
+    const bgColor = bgMap[tasks[0].task_priority] ?? "";
+
+    return (
+        <div className='w-[467px] h-[82px] bg-[#212121] rounded-[10px] flex flex-row'>
+            <span className='bg-[#7e7e7e] h-auto w-[20px] rounded-l-[10px]'></span>
+            <div className='w-full h-full pl-[15px] pr-[13px] py-[15px]'>
+                <div className='w-full h-full '>
+                    <p className='font-helvetica font-medium text-white text-[20px]'>{tasks[0].task_name}</p>
+                    <span className='flex flex-row'>
+                        <p className='font-helvetica font-normal text-[#ADADAD] text-[14px] '>Complete by: {tasks[0].task_completion_date}</p>
+                        <p className='font-helvetica font-normal text-[#ADADAD] text-[14px] ml-[10px]'>|</p>
+                        <div className={`px-[6px] inline-flex items-center justify-center w-auto h-auto rounded-[15px] ml-[12px] ${bgColor}`}>
+                            <p className='font-helvetica font-bold text-white text-[10px]'>{priority}</p>
+                        </div>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Task
