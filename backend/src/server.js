7a7414f51d81ecccedb784ef0766651312cfcbe0
Nawshhh <wilbertshawnvillagarcia@gmail.com>
2025-08-17 01:23:33 +0800

    feat: finished working on backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,23 @@ import express from "express";
 import taskRoutes from "./routes/taskRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-connectDB();
-
 app.use(express.json()); // middleware function
+
+app.use(rateLimiter);
 
 app.use("/smartlist", taskRoutes);
 
-app.listen(PORT, () => {
-    console.log("Server started on PORT: 5001");
+// connect first and then put the console log of listening inside the db
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on PORT: 5001");
+    });
 });
+
